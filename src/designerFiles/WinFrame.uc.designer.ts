d9365ca25c648625b823e5b2a4bbf0a8548c5df9
pratik2201 <pratik2201.pp@gmail.com>
2026-02-21 12:33:48 +0530

    Initial

---
+++ src/designerFiles/WinFrame.uc.designer.ts
@@ -0,0 +1,82 @@
+ import { Usercontrol,intenseGenerator,IUcOptions,ResourceManage } from "uc-runtime/core.js";
+ import { WinFrame } from "./../WinFrame.uc.js";
+
+
+export class WinFrame$Designer extends Usercontrol {    
+    
+    get(id:"") {
+        return this.ucExtends.find(`[id="${id}"]`)[0];
+    }
+    public titlebar1!: HTMLElement;
+    public lbl_title!: HTMLElement;
+    public cmd_restore!: HTMLElement;
+    public cmd_max!: HTMLElement;
+    public cmd_close!: HTMLElement;
+    public container1!: HTMLElement;
+    
+    
+    constructor(){ super(); }
+
+    
+
+    static Create(pera: IUcOptions, ...args: any[]) {
+       return ( intenseGenerator.generateUC(WinFrame,  pera, ...args)) as WinFrame;
+    }
+
+    initializecomponent?(args: IUcOptions, form: WinFrame){
+        const ucExt = this.ucExtends;
+        args.guid = "uc-controls:ed24c044-c04d-4a5b-9a24-0f77b14ddfda:00000004";
+        args.htmlContent = ResourceManage.getContent("uc-controls:ed24c044-c04d-4a5b-9a24-0f77b14ddfda:00000003");
+        args.cssContent = ResourceManage.getContent("uc-controls:ed24c044-c04d-4a5b-9a24-0f77b14ddfda:00000004");
+
+        ucExt.initializecomponent(args);                
+        const CONTROLS = ucExt.controls; 
+
+        
+        this.titlebar1 = CONTROLS.titlebar1  as unknown as HTMLElement;
+        this.lbl_title = CONTROLS.lbl_title  as unknown as HTMLElement;
+        this.cmd_restore = CONTROLS.cmd_restore  as unknown as HTMLElement;
+        this.cmd_max = CONTROLS.cmd_max  as unknown as HTMLElement;
+        this.cmd_close = CONTROLS.cmd_close  as unknown as HTMLElement;
+        this.container1 = CONTROLS.container1  as unknown as HTMLElement;
+
+        if(args.events?.beforeFinalize!=undefined) args.events?.beforeFinalize(form);
+        ucExt.finalizeInit(args);        
+        delete this.initializecomponent; 
+        delete this.initializecomponentAsync; 
+        ucExt.takeoff();
+    }
+
+
+
+    static  async CreateAsync(pera: IUcOptions, ...args: any[]) {
+       return ( await  intenseGenerator.generateUCAsync(WinFrame,  pera, ...args)) as WinFrame;
+    }
+
+     async initializecomponentAsync?(args: IUcOptions, form: WinFrame){
+        const ucExt = this.ucExtends;
+        args.guid = "uc-controls:ed24c044-c04d-4a5b-9a24-0f77b14ddfda:00000004";
+        args.htmlContent = ResourceManage.getContent("uc-controls:ed24c044-c04d-4a5b-9a24-0f77b14ddfda:00000003");
+        args.cssContent = ResourceManage.getContent("uc-controls:ed24c044-c04d-4a5b-9a24-0f77b14ddfda:00000004");
+
+        ucExt.initializecomponent(args);                
+        const CONTROLS = ucExt.controls; 
+
+        
+        this.titlebar1 = CONTROLS.titlebar1  as unknown as HTMLElement;
+        this.lbl_title = CONTROLS.lbl_title  as unknown as HTMLElement;
+        this.cmd_restore = CONTROLS.cmd_restore  as unknown as HTMLElement;
+        this.cmd_max = CONTROLS.cmd_max  as unknown as HTMLElement;
+        this.cmd_close = CONTROLS.cmd_close  as unknown as HTMLElement;
+        this.container1 = CONTROLS.container1  as unknown as HTMLElement;
+
+        if(args.events?.beforeFinalize!=undefined) args.events?.beforeFinalize(form);
+        ucExt.finalizeInit(args);        
+        delete this.initializecomponent; 
+        delete this.initializecomponentAsync; 
+        ucExt.takeoff();
+    }
+
+
+}
+
